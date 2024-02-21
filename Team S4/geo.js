90da1c2d6bc0d3df7f2c80af6aed44421574c2ab
Tarsh Swarnkar <123285559+T1A0R3S2H@users.noreply.github.com>
2024-02-21 22:19:41 +0530

    Delete Team S4 directory

--- Team S4/geo.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("searchBtn").addEventListener("click", function () {
-    country = document.getElementById("search").value
-    document.querySelectorAll(`.allPaths`).forEach(e => {
-        e.style.fill = "#ececec"
-    })
-    document.querySelectorAll(`#${country}`).forEach(e => {
-        e.style.fill = "red"
-    })
-});
